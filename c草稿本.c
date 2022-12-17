d1b6c948ca61dbfa265f103d0fc29b8648cb46b2
黄武豪 <@STAR>
2022-12-17 14:40:12 +0800

    添加项目文件。

---
+++ c草稿本.c
@@ -0,0 +1,60 @@
+//#include<stdio.h>
+//int main(void)
+//{
+//	int a = 80;
+//	printf("比特\n");
+//	printf("hehe\n");
+//	printf("%d\n", 100);
+//	printf("%d", sizeof(int));
+//
+//	return 0;
+//}
+
+
+
+//#include<stdio.h>
+//int main(void)
+//{
+//	int age = 20;//定义年龄。
+//	double weight = 65;//定义身高。
+//
+//	age = age + 1;
+//	weight = weight + 5;
+//
+//	printf("%d\n", age);//整形用%d来表示。
+//	printf("%lf\n", weight);//double要用%lf来表示。
+//
+//	return 0;
+//}
+
+
+
+//#include<stdio.h>
+//int a = 100;//这是一个全局变量。
+//int main(void)
+//{
+//	int a = 20;//这是一个局部变量。
+//	printf("%d", a);
+//	return 0;
+//	//当局部变量与全局变量名字冲突时，局部变量优先。
+//	//不建议把全局变量和局部变量的名字写成一样的。
+//}
+
+
+
+
+//写一个代码，求两数的和。
+#include<stdio.h>
+int main(void)
+{
+	int a = 56;
+	int b = 44;
+	int c = 0;
+
+	c = a + b;
+
+	printf("a,b两数之和为%d", c);
+
+
+	return 0;
+}
