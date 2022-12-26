510268338ab70fbe1682216ea207ebe311fb42f4
黄武豪 <1502114880@qq.com>
2022-12-26 20:54:39 +0800

    更改一次

--- test_12_26.c
+++ test_12_26.c
@@ -1,1 +1,44 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+// int Max(int x,int y)
+//{
+//	int z = 0;
+//	return x > y ? x : y;
+//}
+//int main()
+//{
+//	int a = 0 ;
+//	int b = 0 ;
+//	scanf("%d %d", &a, &b);
+//	printf("%d\n",Max(a,b));
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	int m = 0;
+//	scanf("%d", &m);
+//	
+//	m % 5 == 0 ? printf("YES") : printf("NO");//ÈýÄ¿²Ù×÷·û¡£
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	int m = 0;
+//	scanf("%d", &m);
+//	if (m % 5 == 0)
+//	{
+//		printf("YES");
+//	}
+//
+//	else
+//	{
+//		printf("NO");
+//	}
+//	return 0;
+//}
