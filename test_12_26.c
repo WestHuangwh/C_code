aca6fbd2521366c21614647e0196784e12b6b6fe
黄武豪 <1502114880@qq.com>
2022-12-27 16:37:33 +0800

    更改一次

--- test_12_26.c
+++ test_12_26.c
@@ -98,17 +98,83 @@
 
 
 
+//#include<stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1; i < 100; i++)
+//	{
+//		if (i % 2 == 1)
+//		{
+//			printf("%d, ", i);
+//		}
+//	}
+//	
+//	return 0;
+//}
+
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1; i <= 100; i += 2)
+//	{
+//		printf("%d ", i);
+//	}
+//	return 0;
+//}
+
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	int day = 0;
+//	scanf("%d", &day);
+//	switch (day)
+//	{
+//	case 1:
+//		printf("星期一\n");
+//		break;
+//	case 2:
+//		printf("星期二\n");
+//		break;
+//	case 3:
+//		printf("星期三\n");
+//		break;
+//	case 4:
+//		printf("星期四\n");
+//		break;
+//	case 5:
+//		printf("星期五\n");
+//		break;
+//	case 6:
+//		printf("星期六\n");
+//		break;
+//	case 7:
+//		printf("星期天\n");
+//		break;
+//	default:
+//		printf("输入错误\n");
+//		break;
+//	}
+//	return 0;
+//}
+
+
+
 #include<stdio.h>
 int main()
 {
-	int i = 0;
-	for (i = 1; i < 100; i++)
+	int i = 1;
+	while (i <= 10)
 	{
-		if (i % 2 == 1)
-		{
-			printf("%d, ", i);
-		}
+		if (5 == i)
+			break;//在while循环里，break用于永久的终止循环。
+		printf("%d ", i);
+		i++;
 	}
-	
 	return 0;
 }
