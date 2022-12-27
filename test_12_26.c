416aeb62dbc525201b19703c734cb2712c1c5f0a
黄武豪 <1502114880@qq.com>
2022-12-27 22:18:15 +0800

    更改一次

--- test_12_26.c
+++ test_12_26.c
@@ -259,13 +259,42 @@
 
 
 
+//#include<stdio.h>
+//int main()
+//{
+//	int i = 1;//≥ı ºªØ
+//	while (i <= 10)
+//	{
+//		printf("%d ", i);
+//		i++;
+//	}
+//	return 0;
+//}
+
+
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	int i = 1;
+//	for (i = 1; i <= 10; i++)
+//	{
+//		printf("%d ", i);
+//	}
+//	return 0;
+//}
+
+
+
 #include<stdio.h>
 int main()
 {
-	int i = 1;
+	int i = 0;
 	for (i = 1; i <= 10; i++)
 	{
 		printf("%d ", i);
 	}
+
 	return 0;
 }
