5188a6e6c145fa8f862569a3cf39e9f04142798b
黄武豪 <1502114880@qq.com>
2022-12-28 17:50:44 +0800

    更改一次

--- test_12_26.c
+++ test_12_26.c
@@ -287,14 +287,44 @@
 
 
 
+//#include<stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1; i <= 10; i++)
+//	{
+//		printf("%d ", i);
+//	}
+//
+//	return 0;
+//}
+
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	int j = 0;
+//	for (i = 0; i < 3; i++)//i的每一次循环就要循环三次j
+//	{
+//		for (j = 0; j < 3; j++)
+//		{
+//			printf("你好\n");
+//		}
+//	}
+//	return 0;
+//}
+
+
+
 #include<stdio.h>
 int main()
 {
 	int i = 0;
-	for (i = 1; i <= 10; i++)
-	{
-		printf("%d ", i);
-	}
+	int k = 0;
+	for (i = 0, k = 0; k == 0; i++, k++)
+		k++;
 
 	return 0;
 }
