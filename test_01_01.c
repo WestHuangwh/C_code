9a07c01d0c6f4942e15357a2997806b6836b5bb5
WestHuangwh <1502114880@qq.com>
2023-01-02 14:20:03 +0800

    更改一次

--- test_01_01.c
+++ test_01_01.c
@@ -43,4 +43,86 @@
 
 
 
+////写一个函数判断是否为闰年。
+//#include<stdio.h>
+//int get_n(int x)
+//{
+//	if (x % 4 == 0 && x %100 != 0||(x % 400 == 0))
+//	{
+//		return 1;
+//	}
+//		return 0;
+//
+//}
+//int main()
+//{
+//	int i = 0;
+//	int consit = 0;
+//	for (i = 1000; i <= 2000; i++)
+//	{
+//		if (get_n(i)==1)
+//		{
+//			printf("%d ", i);
+//			consit++;
+//		}
+//	}
+//	printf("\n闰年个数为：%d", consit);
+//	return 0;
+//}
+
+
+
+
+////每调用一次函数就会将num的值加1
+//#include<stdio.h>
+//void Add(int* p)
+//{
+//	(*p)++;
+//}
+//int main()
+//{
+//	int num = 0;
+//	Add(&num);
+//
+//	printf("%d\n", num);
+//
+//	Add(&num);
+//
+//	printf("%d\n", num);
+//
+//	Add(&num);
+//
+//	printf("%d\n", num);
+//
+//	return 0;
+//}
+
+
+
+//#include<stdio.h>
+//#include<string.h>
+//int main()
+//{
+//	char arr1[20] = { 0 };
+//	char arr2[] = "bit";
+//	
+//	printf("%s\n", strcpy(arr1, arr2));//函数的链式访问
+//
+//	return 0;
+//}
+
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	printf("%d", printf("%d", printf("%d", 43)));//函数返回的是打印在屏幕上字符的个数
+//
+//	
+//
+//	return 0;
+//}
+//
+
+
 
