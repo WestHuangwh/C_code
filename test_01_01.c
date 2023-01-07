32f91f40b4d0e12affb33c5ec59503d13136cb9c
WestHuangwh <1502114880@qq.com>
2023-01-07 21:41:41 +0800

    更改一次

--- test_01_01.c
+++ test_01_01.c
@@ -487,16 +487,16 @@
 
 
 
-#include<stdio.h>
-int main(void)
-{
-	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
-	int* p = arr;//数组名是数组第一个元素的地址
-	int i = 0;
-	for (i = 0; i < 10; i++)
-	{
-		printf("%d ", *p);
-		p++;
-	}
-	return 0;
-}
+//#include<stdio.h>
+//int main(void)
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int* p = arr;//数组名是数组第一个元素的地址
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", *p);
+//		p++;
+//	}
+//	return 0;
+//}
