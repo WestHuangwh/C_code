55ce5db3913f119d2ead1608a8eb4f8d2e5dabcf
WestHuangwh <1502114880@qq.com>
2023-01-10 18:22:37 +0800

    更改一次

--- test_01_01.c
+++ test_01_01.c
@@ -504,6 +504,61 @@
 
 
 
+//#include<stdio.h>
+//int main()
+//{
+//	int arr[][4] = { {1,2},{3,4},{4,5} };
+//	int i = 0;
+//	int j = 0;
+//	for (i = 0; i < 3; i++)
+//	{
+//		for (j = 0; j < 4; j++)
+//		{
+//			printf("$arr[%d][%d] = %p\n", i, j, &arr[i][j]);
+//			 
+//		}
+//
+//	}
+//	return 0;
+//
+
+
+
+////冒泡排序从小到大排序
+//#include<stdio.h>
+//void bubble_sort(int arr[], int sz)//形参arr本质是指针
+//{
+//	//确定趟数
+//	int i = 0;
+//	for (i = 0; i < sz - 1; i++)
+//	{
+//		//一趟冒泡排序的过程
+//		int j = 0;
+//		for (j = 0; j < sz - 1 - i; j++)
+//		{
+//			if (arr[j] > arr[j + 1])
+//			{
+//				//交换
+//				int tmp = arr[j];
+//				arr[j] = arr[j + 1];
+//				arr[j + 1] = tmp;
+//			}
+//		}
+//	}
+//}
+//int main()
+//{
+//	int arr[] = { 9,8,7,6,5,4,3,2,1,0 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	bubble_sort(arr, sz);
+//
+//	return 0;
+//}
+
+
+
+
+
 #include<stdio.h>
 int main()
 {
