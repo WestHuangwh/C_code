172ce7b84c1078088768f216afecaeabb6f7e9ea
WestHuangwh <1502114880@qq.com>
2023-01-15 12:46:00 +0800

    更改一次

--- test_01_15.c
+++ test_01_15.c
@@ -1,6 +1,21 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
 #include<stdio.h>
+int Add(int n)
+{
+	if (n > 0 && n <= 10)
+	{
+		return 1;
+	}
+	else if (n >= 11 && n <= 20)
+	{
+		return 2;
+	}
+	else if (n >= 21 && n <= 40)
+	{
+		return 3;
+	}
+}
 int main()
 {
 	int n = 0;
@@ -10,34 +25,24 @@ int main()
 
 	if (n >= 0 && n <= 40)
 	{
-		input = 1;
-	}
-	if (input)
-	{
-		if (n > 0 && n <= 10)
+		int input = Add(n);
+		if (input)
 		{
-			return 1;
+			
+			switch (input)
+			{
+			case 1:
+				num = 6 * n;
+				break;
+			case 2:
+				num = 6 * 10 + (n - 10) * 2;
+				break;
+			case 3:
+				num = (6 * 10) + (10 * 2) + (n - 20);
+				break;
+			}
 		}
-		else if (n >= 11 && n <= 20)
-		{
-			return 2;
-		}
-		else if(n>=21&&n<=40)
-		{
-			return 3;
-		}
-		switch (input)
-		{
-		case 1:
-			num = 6 * n;
-			break;
-		case 2:
-			num = 6 * n + (n - 10) * 2;
-			break;
-		case 3:
-			num = (6 * n )+ (10 * 2)+(n-20);
-			break;
-		}
+
 		printf("%d", num);
 	}
 	return 0;
