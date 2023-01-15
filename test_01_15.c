1a95b69795685eff689377d9385d70b43282b376
WestHuangwh <1502114880@qq.com>
2023-01-15 12:56:32 +0800

    更改一次

--- test_01_15.c
+++ test_01_15.c
@@ -1,49 +1,57 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
+//#include<stdio.h>
+//int Add(int n)
+//{
+//	if (n > 0 && n <= 10)
+//	{
+//		return 1;
+//	}
+//	else if (n >= 11 && n <= 20)
+//	{
+//		return 2;
+//	}
+//	else if (n >= 21 && n <= 40)
+//	{
+//		return 3;
+//	}
+//}
+//int main()
+//{
+//	int n = 0;
+//	int input = 0;
+//	int num = 0;
+//	scanf("%d", &n);
+//
+//	if (n >= 0 && n <= 40)
+//	{
+//		int input = Add(n);
+//		if (input)
+//		{
+//			
+//			switch (input)
+//			{
+//			case 1:
+//				num = 6 * n;
+//				break;
+//			case 2:
+//				num = 6 * 10 + (n - 10) * 2;
+//				break;
+//			case 3:
+//				num = (6 * 10) + (10 * 2) + (n - 20);
+//				break;
+//			}
+//		}
+//
+//		printf("%d", num);
+//	}
+//	return 0;
+//}
+
+
 #include<stdio.h>
-int Add(int n)
-{
-	if (n > 0 && n <= 10)
-	{
-		return 1;
-	}
-	else if (n >= 11 && n <= 20)
-	{
-		return 2;
-	}
-	else if (n >= 21 && n <= 40)
-	{
-		return 3;
-	}
-}
 int main()
 {
-	int n = 0;
-	int input = 0;
-	int num = 0;
-	scanf("%d", &n);
 
-	if (n >= 0 && n <= 40)
-	{
-		int input = Add(n);
-		if (input)
-		{
-			
-			switch (input)
-			{
-			case 1:
-				num = 6 * n;
-				break;
-			case 2:
-				num = 6 * 10 + (n - 10) * 2;
-				break;
-			case 3:
-				num = (6 * 10) + (10 * 2) + (n - 20);
-				break;
-			}
-		}
-
-		printf("%d", num);
-	}
 	return 0;
 }
