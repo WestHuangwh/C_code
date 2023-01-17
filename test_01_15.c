009d9d3f22c9b422dad8b73b5d8bcf27b3bdd61f
WestHuangwh <1502114880@qq.com>
2023-01-17 12:51:50 +0800

    更改一次

--- test_01_15.c
+++ test_01_15.c
@@ -105,9 +105,28 @@
 
 
 #include<stdio.h>
+int Fib(int n)
+{
+	int a = 1;
+	int b = 1;
+	int c = 1;
+	while (n > 2)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+		n--;
+	}
+	return c;
+}
 int main()
 {
-	int i = 0;
-	
+	int n = 0;
+	scanf("%d", &n);
+	if (n > 2 && n < 30)
+	{
+		printf("%d\n", Fib(n));
+	}
+
 	return 0;
 }
