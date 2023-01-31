e14c5338cec7f7cc3efa5cf37ddc28bde7f38461
黄武豪 <@STAR>
2023-01-31 23:56:26 +0800

    更改一次

--- test_01_25.c
+++ test_01_25.c
@@ -496,9 +496,46 @@
 
 
 #include<stdio.h>
+#include<string.h>
+void rever(char*left, char*right)
+{
+	while (left < right)
+	{
+		char tmp = *right;
+		*right = *left;
+		*left = tmp;
+		left++;
+		right--;
+	}
+	
+}
 int main()
 {
-	
+	char arr[20] = { 0 };
+	gets(arr);
+	int len = strlen(arr);
+	rever(arr, arr + len - 1);
+	char* start = arr;
+	while (*start)
+	{
+		char* end = start;
+		while (*end != ' ' && *end != '\0')
+		{
+			end++;
+		}
+
+		rever(start, end - 1);
+		if (*end == ' ')
+		{
+			start = end+1;
+		}
+		else
+		{
+			start = end;
+		}
+
+	}
+	printf("%s", arr);
 	
 	return 0;
 }
