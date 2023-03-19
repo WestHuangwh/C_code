f00722495cfac227c603cb47c30e8f36c819e4d1
WestHuangwh <1502114880@qq.com>
2023-03-19 23:16:12 +0800

    更改一次

--- test_03_10.c
+++ test_03_10.c
@@ -265,40 +265,50 @@
 
 
 
+////有序序列中插入一个数
+//#include<stdio.h>
+//int main(void)
+//{
+//	int arr[51] = { 0 };
+//	int n = 0;
+//	scanf("%d", &n);
+//	int i = 0;
+//	for (i = 0; i < n; i++)
+//	{
+//		scanf("%d", &arr[i]);
+//	}
+//	int m = 0;
+//	scanf("%d", &m);
+//	for (i = n - 1; i >= 0; i--)
+//	{
+//		if (arr[i] > m)
+//		{
+//			arr[i + 1] = arr[i];
+//
+//		}
+//		else
+//		{
+//			arr[i + 1] = m;
+//			break;
+//
+//		}
+//	}
+//	if (i < 0)
+//	{
+//		arr[0] = m;
+//	}
+//	for (i = 0; i <n+1; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	return 0;
+//}
+
+
+
 #include<stdio.h>
 int main(void)
 {
-	int arr[51] = { 0 };
-	int n = 0;
-	scanf("%d", &n);
-	int i = 0;
-	for (i = 0; i < n; i++)
-	{
-		scanf("%d", &arr[i]);
-	}
-	int m = 0;
-	scanf("%d", &m);
-	for (i = n - 1; i >= 0; i--)
-	{
-		if (arr[i] > m)
-		{
-			arr[i + 1] = arr[i];
 
-		}
-		else
-		{
-			arr[i + 1] = m;
-			break;
-
-		}
-	}
-	if (i < 0)
-	{
-		arr[0] = m;
-	}
-	for (i = 0; i <n+1; i++)
-	{
-		printf("%d ", arr[i]);
-	}
 	return 0;
 }
