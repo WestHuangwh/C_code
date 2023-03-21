8a7307157a7fdd166dae6f7617a5a8d5dd6c4e19
WestHuangwh <1502114880@qq.com>
2023-03-21 13:14:44 +0800

    更改一次

--- test_03_10.c
+++ test_03_10.c
@@ -306,9 +306,3 @@
 
 
 
-#include<stdio.h>
-int main(void)
-{
-
-	return 0;
-}
---
+++ test_03_19.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+#include<stdio.h>
+int main(void)
+{
+	//打开文件
+	FILE* pf = fopen("test.dat", "w");
+	if (pf == NULL)//对返回的指针进行判断
+	{
+		perror("fopen");
+		return 1;
+	}
+	//写文件
+
+
+	//关闭文件
+	fclose(pf);
+	pf = NULL;
+	return 0;
+}
