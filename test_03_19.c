8683fb13aa142be0372c67f531816074ac4dafef
WestHuangwh <1502114880@qq.com>
2023-03-25 00:05:19 +0800

    更改一次

--- test_03_19.c
+++ test_03_19.c
@@ -174,18 +174,121 @@
 
 
 
+//#include<stdio.h>
+//int main(void)
+//{
+//	int i = 0;
+//	for (i = 1; i <= 9; i++)
+//	{
+//		int j = 0;
+//		for (j = 1; j <= i; j++)
+//		{
+//			printf("%d*%d=%d ", j, i, i * j);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+
+
+//#include<stdio.h>
+//#include<stdlib.h>
+//#include<time.h>
+//void nunm()
+//{
+//	printf("****************************************\n");
+//	printf("***********1.开始游戏       ************\n");
+//	printf("***********0.退出游戏       ************\n");
+//	printf("****************************************\n");
+//	
+//}
+//void game()
+//{
+//	int ret = rand() % 100 + 1;
+//	//printf("%d",red);
+//	int guts = 0;
+//	while (1)
+//	{
+//		printf("请猜数字:> ");
+//		scanf("%d", &guts);
+//		if (guts < ret)
+//		{
+//			printf("猜小了\n");
+//		}
+//		else if (guts > ret)
+//		{
+//			printf("猜大了\n");
+//		}
+//		else
+//		{
+//			printf("恭喜你猜对了\n");
+//			break;
+//		}
+//	}
+//}
+//int main()
+//{
+//	int a = 0;
+//	srand((unsigned int)time(NULL));
+//	do
+//	{
+//		nunm();
+//		printf("请选择:> ");
+//		scanf("%d", &a);
+//
+//		switch (a)
+//		{
+//		case 1:
+//			printf("猜数字游戏开始\n");
+//			game();
+//			break;
+//		case 0:
+//			printf("退出游戏\n");
+//			break;
+//		default:
+//			printf("输入错误，请重新输入\n");
+//		}
+//
+//	} while (a);
+//	return 0;
+//
+//}
+
+
+
 #include<stdio.h>
 int main(void)
 {
-	int i = 0;
-	for (i = 1; i <= 9; i++)
-	{
-		int j = 0;
-		for (j = 1; j <= i; j++)
-		{
-			printf("%d*%d=%d ", j, i, i * j);
+
+	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int k = 0;
+	printf("请输入您需要查找的数字:>");
+	scanf("%d", &k);
+	int left = 0;
+	int right = sz - 1;
+
+	while (left <= right) {
+
+		int mid = (left + right) / 2;
+
+		if (arr[mid] < k) {
+			left = mid + 1;
 		}
-		printf("\n");
+		else if (arr[mid] > k) {
+			right = mid - 1;
+		}
+		else if (arr[mid] == k) {
+			printf("找到了，该元素下标是：%d\n", mid);
+			break;
+		}
 	}
+
+	if (left > right) {
+		printf("对不起，没有找到该元素\n");
+	}
+
 	return 0;
+
 }
