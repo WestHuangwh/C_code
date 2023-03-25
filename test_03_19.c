d9cb90914403f36fbdeec65ab0270eebb001252a
WestHuangwh <1502114880@qq.com>
2023-03-25 16:50:13 +0800

    更改一次

--- test_03_19.c
+++ test_03_19.c
@@ -466,23 +466,47 @@
 //}
 
 
+//#include<stdio.h>
+//int DigitSum(int n)
+//{
+//	if (n < 10)
+//	{
+//		return n;
+//	}
+//	else
+//	{
+//		
+//		return DigitSum(n / 10)+n%10;
+//	}
+//}
+//int main(void)
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	printf("%d\n",DigitSum(n));
+//	return 0;
+//}
+
+
 #include<stdio.h>
-int DigitSum(int n)
+int Sgin(int n, int k)
 {
-	if (n < 10)
+	if (k == 0)
 	{
-		return n;
+		return 1;
 	}
-	else
+
+	if (k>=1)
 	{
-		
-		return DigitSum(n / 10)+n%10;
+		return Sgin(n,k - 1) * n;
 	}
 }
 int main(void)
 {
 	int n = 0;
-	scanf("%d", &n);
-	printf("%d\n",DigitSum(n));
+	int k = 0;
+	scanf("%d %d", &n, &k);
+	int ret = Sgin(n, k);
+	printf("%d\n", ret);
 	return 0;
 }
