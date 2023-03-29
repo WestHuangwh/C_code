61fced98223e970d416b480c3661baa847883f50
WestHuangwh <1502114880@qq.com>
2023-03-29 15:39:53 +0800

    更改一次a

--- test_03_19.c
+++ test_03_19.c
@@ -641,4 +641,23 @@
 //	return 0;
 //}
 
-
+#include <stdio.h> 
+void main() 
+{
+	int a, b;
+	unsigned c, d;
+	long e, f;
+	a = 100;
+	b = -100;
+	e = 50000;
+	f = 32767;
+	c = a;
+	d = b;
+	printf("%d,%d\n", a, b);
+	printf(" %u, %u\n ", a, b);
+	printf("%u,%u\n", c, b);
+	c = a = e;
+	d = b = f;
+	printf("%d,%d\n", a, b);
+	printf("%u,%u\n", c, b);
+}
